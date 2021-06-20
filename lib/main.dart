2d78fcc9733cc81bd2678812aea298120b193de2
Saiterxx <victormbeck1234@gmail.com>
2021-06-20 19:56:38 -0300

    commit 2 - parte 1 da implementação do firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -94,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'poggers:',
+              'mano:',
             ),
             Text(
               '$_counter',
