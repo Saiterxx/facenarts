241ff66ab1c4ff2c70634f7385a55c249a5c2f13
Saiterxx <victormbeck1234@gmail.com>
2021-06-23 18:51:31 -0300

    Commit 10 - Update de username

--- lib/SettingsScreen.dart
+++ lib/SettingsScreen.dart
@@ -1,12 +1,79 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:facenarts/services/database.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:facenarts/services/database.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
+  @override
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  late String _newusername;
+  final auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+
+    Widget myFirebaseForm() {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //1.)
+          Text('Insira seu novo nome de usuário'),
+          TextField(
+            decoration: InputDecoration(icon: Icon(Icons.person),hintText: ""),
+            onChanged: (value) {
+              setState(() {
+                _newusername = value.trim(); //remove whitespace
+              });
+            },
+          ),
+          //2.)
+          //3.)
+          // ignore: deprecated_member_use
+          RaisedButton(
+                    child: Text("Atualizar"),
+                    onPressed: () async {
+                      try {
+                        // ignore: unnecessary_null_comparison
+                        if(auth!=null){
+                          print(auth.currentUser!.email.toString());
+                          addName(_newusername, auth.currentUser!.email.toString());
+
+                          Navigator.of(context).pushNamed("/logged");
+                          ScaffoldMessenger.of(context)
+                        .showSnackBar(SnackBar(content: Text(
+                        'Nome de Usuário atualizado com sucesso.'
+                      )));
+                        }
+                        else{
+                          print("Erro no Login ");
+                        }
+
+                      } on FirebaseAuthException catch (e) {
+                        print(e);
+                      }
+                    }),
+
+          //4.)
+          // ignore: deprecated_member_use
+        ],
+      ),
+    );
   }
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("FacenArts"),
+        ),
+        body: myFirebaseForm());
+  }
+
+  Future addName(String _newusername, String email) async{
+    await DatabaseService(email).updateUserData(_newusername, email);
+  }
+
+
+
 }
